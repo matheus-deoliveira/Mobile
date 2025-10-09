def19e30c0c2ee201b4ed47cc8a6a4a885cd58e4
Matheus <matheusdeoliveiraes@gmail.com>
2025-10-09 16:54:36 -0300

    Tendo problema para rodar o código com os argumentos.

---
+++ PrimeiroMiniProjeto/LojaArtigoNerds/src/main/kotlin/Eletronico.kt
@@ -0,0 +1,17 @@
+package org.example
+
+class Eletronico : Produto() {
+    enum class Tipo
+    {
+        VIDEO_GAME,
+        JOGO,
+        PORTATIL,
+        OUTROS
+    }
+
+    lateinit var tipo: Tipo
+
+    var versao: String = ""
+
+    var anoDeFabricacao: Int = 0
+}
