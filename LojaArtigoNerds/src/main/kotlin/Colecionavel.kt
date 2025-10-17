1d92cdbed951d6eb468bf54e1ec981059023407a
Matheus de Oliveira <matheusdeoliveiraes@gmail.com>
2025-10-17 11:43:28 -0300

    Unversioned Files (vou commitar pq ele tá deletando não sei porque)

---
+++ LojaArtigoNerds/src/main/kotlin/Colecionavel.kt
@@ -0,0 +1,36 @@
+package org.example
+
+class Colecionavel : Produto() {
+
+    enum class Tipo
+    {
+        LIVRO,
+        BONECO,
+        OUTROS
+    }
+
+    lateinit var tipo: Tipo
+
+    enum class MaterialFabricacao
+    {
+        PAPEL,
+        PLASTICO,
+        ACO,
+        MISTURADO,
+        OUTROS
+    }
+
+    lateinit var materialFabricacao: MaterialFabricacao
+
+    var tamanhoEmCentimetros: Int = 0
+
+    enum class Relevancia
+    {
+        COMUM,
+        MEDIO,
+        RARO,
+        RARISSIMO
+    }
+
+    lateinit var relevancia: Relevancia
+}
---
+++ LojaArtigoNerds/src/main/kotlin/Produto.kt
@@ -0,0 +1,8 @@
+package org.example
+
+open class Produto {
+    var nome: String = ""
+    var precoComprado: Double = 0.0
+    var precoVendido: Double = 0.0
+    var code: String = ""
+}
