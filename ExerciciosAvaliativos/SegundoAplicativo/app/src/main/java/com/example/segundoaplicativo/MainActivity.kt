19d66ccb2c38db7d69aaa10df153ef0fbd3af737
Matheus <matheusdeoliveiraes@gmail.com>
2025-11-06 14:28:12 -0300

    Adicionando tudo

---
+++ ExerciciosAvaliativos/SegundoAplicativo/app/src/main/java/com/example/segundoaplicativo/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.segundoaplicativo
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.segundoaplicativo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.guardarButton.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == binding.guardarButton.id) {
+            val nome = binding.nomeFieldEditText.text.toString()
+
+            val intent = Intent(this, CatDogActivity::class.java)
+
+            startActivity(intent)
+        }
+    }
+}
