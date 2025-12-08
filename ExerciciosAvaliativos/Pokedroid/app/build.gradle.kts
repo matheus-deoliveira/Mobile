44cc4ba4a0c1a44aa4d5ddd470abf27ecc083611
Matheus de Oliveira <matheusdeoliveiraes@gmail.com>
2025-12-08 08:48:56 -0300

    feat: configurando o build.gradle.kts e o AndroidManifest.xml

--- ExerciciosAvaliativos/Pokedroid/app/build.gradle.kts
+++ ExerciciosAvaliativos/Pokedroid/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
 }
 
 android {
@@ -35,6 +36,9 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -46,4 +50,20 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // ViewModel e LiveData
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.10.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.10.0")
+
+    // Retrofit (Consumo de API) & GSON (Conversor de JSON)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // Glide (Para carregar imagens das URLs)
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    // Room (Banco de Dados Local para Favoritos)
+    implementation("androidx.room:room-runtime:2.6.0")
+    implementation("androidx.room:room-ktx:2.6.0") // Suporte a Coroutines
+    kapt("androidx.room:room-compiler:2.6.0") // Processador de anotações
 }
